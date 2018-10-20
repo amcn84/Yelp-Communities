85cceff959c76e168b2b0643f0d1c0d9195dda25
Allen McNichols <allen.mcnichols@webdevstudios.com>
2018-10-19 17:07:59 -0700

    Add License

--- yelp-communities.php
+++ yelp-communities.php
@@ -8,6 +8,20 @@
  *
  * @since October 18, 2018
  * @package yelp-communities
+ *
+ * License:     GPL2
+ * Yelp Communities is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * Yelp Communities is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Yelp Communities. If not, see {License URI}.
  */
 
 defined( 'ABSPATH' ) || die( 'Say What?' );
